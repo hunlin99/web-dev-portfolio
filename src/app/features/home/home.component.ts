eb1f74c8d5380a206d9e10ba2b2f47c14cdd1d52
hunlin99 <hunlin99@gmail.com>
2026-07-06 05:15:41 +0900

    complete projects page layout

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 import { HeaderComponent } from '../../layout/header/header.component';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, RouterLinkActive, HeaderComponent, CommonModule],
+  imports: [RouterLinkActive, HeaderComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { HeaderComponent } from '../../layout/header/header.component';
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [HeaderComponent, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
 
+  //section title: Web development and design
+  webProjects = [
+    { title: "DC3: Viral Menace", type: "Full stack Development", Skills: "Figma, Angular,Tailwind, Firebase", year: "2026", image: "images/DC3-Viral-Menace/DC3-Viral-Menace-project-img.jpg", link: "/dc3-viral-menace" },
+    { title: "Cuisines Around Us", type: "Full stack Development", Skills: "React,Bootstrap, SQL", year: "2024", image: "images/Cuisines-Around-Us/cuisines-around-us.png", link: "/cuisines-around-us" },
+    { title: "AMPA Project", type: "Landing Pages Web Design", Skills: "Prototyping, UX Design", year: "2025", image: "images/AMPA/AMPA-project-img.jpg", link: "https://afroidentity.ca/" },
+    { title: "Organic Care", type: "E-commers Web Design", Skills: "Figma, HTML, CSS", year: "2024", image: "images/Organic-Care/organic-care-img.jpg", link: "/organic-care" }
+  ]
+
+  //section title: APP development and design
+  appProjects = [
+    { title: "TracLocal", type: "App design and front end Development", Skills: "Figma, Angular, JavaScript", year: "2025", image: "images/TravLocal/TravLocal-image.jpg", link: "/tracLocal" },
+    { title: "Health Pocket", type: "APP UI Design", Skills: "Figma, Interactive Prototype", year: "2022", image: "images/Health-Pocket/health-pocket-imgpng.png", link: "/health-pocket" },
+  ]
 }
